0f90cbf58b25f985864595fb28ad549f87ca796d
lianshufeng <251708339@qq.com>
2025-05-25 23:42:15 +0800

    update

--- build.cmd
+++ build.cmd
@@ -1,3 +1,3 @@
 set HTTP_PROXY=http://192.168.31.98:1080
 set HTTPS_PROXY=http://192.168.31.98:1080
-docker build ./ -f Dockerfile --build-arg HTTP_PROXY=http://192.168.31.98:1080 --build-arg HTTPS_PROXY=http://192.168.31.98:1080 -t tasker:latest
+docker build ./ -f Dockerfile --build-arg HTTP_PROXY=http://192.168.31.98:1080 --build-arg HTTPS_PROXY=http://192.168.31.98:1080 -t lianshufeng/tasker:latest
