45d66f2e901481c83e878569ecfd6727197e418b
lianshufeng <251708339@qq.com>
2025-07-18 15:06:46 +0800

    优化代码

--- demo/GetPlatformItemInfo/platforms/kuaishou.py
+++ demo/GetPlatformItemInfo/platforms/kuaishou.py
@@ -8,8 +8,9 @@ import traceback
 
 import httpx
 from bs4 import BeautifulSoup
+from playwright.async_api import async_playwright
 
-from .base import PlatformAction, ActionResultItem, Comment
+from .base import PlatformAction, ActionResultItem, Comment, getChromeExecutablePath
 
 # 日志配置，建议你根据生产环境实际需要调整
 logging.basicConfig(
@@ -31,8 +32,8 @@ def filter_duplicate_comments(comments: list[Comment]) -> list[Comment]:
     # Return the list of unique comments
     return list(unique_comments.values())
 
-async def request_comment(item: ActionResultItem, cursor: str = None, max_comment_count: int = 800):
-    cookie: str = os.getenv("SCRIPT_COOKIE", None)
+
+async def request_comment(cookie: str, item: ActionResultItem, cursor: str = None, max_comment_count: int = 800):
     cookie_dict: dict[str, str] = {}
     if cookie is not None:
         cookie_dict = dict(item.strip().split('=', 1) for item in cookie.strip(';').split(';') if item)
@@ -107,6 +108,12 @@ query commentListQuery($photoId: String, $pcursor: String) {
         visionCommentList: dict = data.get("visionCommentList")
         if visionCommentList is None:
             return
+
+        commentCount = visionCommentList.get("commentCount")
+        if commentCount is None:
+            logger.info("评论抓取失败: %s", commentCount)
+            return
+
         item.statistics_comment_count = int(visionCommentList.get("commentCount"))  # 评论总数
         rootComments: list[dict] = visionCommentList.get("rootComments")  # 评论列表
         pcursor: str = visionCommentList.get("pcursor", "")  # 游标
@@ -124,12 +131,62 @@ query commentListQuery($photoId: String, $pcursor: String) {
         logger.info("page_comments : %s/%s", len(item.comments), item.statistics_comment_count)
         if len(item.comments) < max_comment_count and len(item.comments) < item.statistics_comment_count:
             await asyncio.sleep(random.randint(300, 1500) / 1000)
-            await request_comment(item=item, cursor=pcursor, max_comment_count=max_comment_count)
+            await request_comment(cookie=cookie, item=item, cursor=pcursor, max_comment_count=max_comment_count)
 
 
 # 访问网页并提取网页中存在的js对象并转换为dict
-async def request_page(url: str) -> dict:
-    cookie: str = os.getenv("SCRIPT_COOKIE", None)
+async def request_page(url: str) -> [str, dict]:
+
+    # cookies
+    cookies: str = os.getenv("SCRIPT_COOKIE", None)
+
+    # 更新cookies
+    # chrome: list = getChromeExecutablePath()
+    # chrome_path = chrome[0] if chrome else None
+    # if chrome_path is None:
+    #     raise RuntimeError("未找到可用的chrome")
+    # args_list = [
+    #     '--incognito',
+    #     '--disable-infobars',
+    #     '--no-first-run',
+    #     '--no-default-browser-check',
+    #     '--disable-features=ExternalProtocolDialog',
+    #     # '--headless=new' #无头模式
+    # ]
+    # async with async_playwright() as p:
+    #     browser = await p.chromium.launch(
+    #         headless=False,
+    #         executable_path=chrome_path,
+    #         args=args_list
+    #     )
+    #     context = await browser.new_context()
+    #     if cookies:
+    #         cookie_list = [
+    #             {
+    #                 'name': k,
+    #                 'value': v,
+    #                 'domain': '.douyin.com',  # 设置为整个主域
+    #                 'path': '/'  # 通常需要设置 path
+    #             }
+    #             for k, v in (item.strip().split('=', 1) for item in cookies.split(';') if '=' in item)
+    #         ]
+    #         await context.add_cookies(cookie_list)
+    #     page = await context.new_page()
+    #
+    #     await page.goto("https://www.baidu.com")
+    #     await asyncio.sleep(0.2)
+    #     await page.goto(url)
+    #     await asyncio.sleep(1)
+    #     # cookies
+    #     cookies = await page.context.cookies()
+    #     cookies = "; ".join([f"{c['name']}={c['value']}" for c in cookies if '.kuaishou.com' in c.get('domain', '')])
+    #     # 如果出现验证码框直接关掉，保证继续请求
+    #     # 查找所有 iframe
+    #     captcha_iframe = page.locator("iframe[src*='captcha.zt.kuaishou.com']")
+    #     if await captcha_iframe.count() > 0:
+    #         logger.info("有验证码...")
+    #         pass
+    #     await browser.close()
 
     # 自定义请求头（包含浏览器User-Agent）
     headers = {
@@ -137,8 +194,8 @@ async def request_page(url: str) -> dict:
     }
 
     # 增加cookie的支持
-    if cookie is not None:
-        headers["Cookie"] = cookie
+    if cookies is not None:
+        headers["Cookie"] = cookies
 
     # 使用 httpx 获取网页内容
     with httpx.Client(timeout=20) as client:
@@ -161,7 +218,7 @@ async def request_page(url: str) -> dict:
                     apollo_json = json.loads(json_str)
                 break
 
-        return apollo_json
+        return cookies, apollo_json
 
 
 class KuaishouPlatformAction(PlatformAction):
@@ -172,7 +229,7 @@ class KuaishouPlatformAction(PlatformAction):
         # 返回对象
         item: ActionResultItem = ActionResultItem()
 
-        page_object: dict = await request_page(url)
+        cookies, page_object = await request_page(url)
         if page_object is None:
             return None
 
@@ -182,16 +239,15 @@ class KuaishouPlatformAction(PlatformAction):
         visionVideoDetailPhoto_name: dict = next(
             (key for key in defaultClient if key.startswith("VisionVideoDetailPhoto:")),
             None)
-        if visionVideoDetailPhoto_name is None:
-            return None
-        visionVideoDetailPhoto = defaultClient.get(visionVideoDetailPhoto_name)
-        item.id = visionVideoDetailPhoto.get("id")  # id
-        item.title = visionVideoDetailPhoto.get("caption")  # 标题
-        item.statistics_digg_count = int(visionVideoDetailPhoto.get("realLikeCount"))  # 点赞
-        item.create_time = int(int(visionVideoDetailPhoto.get("timestamp")) / 1000)  # 发布时间
-        item.video_url = visionVideoDetailPhoto.get("photoUrl")  # 视频地址
-        item.video_cover_url = visionVideoDetailPhoto.get("coverUrl")  # 封面地址
-        item.video_duration = float(int(visionVideoDetailPhoto.get("duration")) / 1000)
+        if visionVideoDetailPhoto_name is not None:
+            visionVideoDetailPhoto = defaultClient.get(visionVideoDetailPhoto_name)
+            item.id = visionVideoDetailPhoto.get("id")  # id
+            item.title = visionVideoDetailPhoto.get("caption")  # 标题
+            item.statistics_digg_count = int(visionVideoDetailPhoto.get("realLikeCount"))  # 点赞
+            item.create_time = int(int(visionVideoDetailPhoto.get("timestamp")) / 1000)  # 发布时间
+            item.video_url = visionVideoDetailPhoto.get("photoUrl")  # 视频地址
+            item.video_cover_url = visionVideoDetailPhoto.get("coverUrl")  # 封面地址
+            item.video_duration = float(int(visionVideoDetailPhoto.get("duration")) / 1000)
 
         # 作者详情
         visionVideoDetailAuthor_name: dict = next(
@@ -216,7 +272,7 @@ class KuaishouPlatformAction(PlatformAction):
         # 评论
         item.comments = []
         try:
-            await request_comment(item=item, cursor="", max_comment_count=max_comment_count)
+            await request_comment(cookie=cookies, item=item, cursor="", max_comment_count=max_comment_count)
         except Exception as e:
             logger.error(e)
             logger.error("Traceback:\n%s", traceback.format_exc())
@@ -224,7 +280,6 @@ class KuaishouPlatformAction(PlatformAction):
         item.comments = filter_duplicate_comments(item.comments)
 
         logger.info(f"comments size: {len(item.comments)}")
-
 
         return item
 
