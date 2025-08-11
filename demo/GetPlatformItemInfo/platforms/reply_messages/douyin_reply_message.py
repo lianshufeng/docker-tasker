ea9b58935dc3f6f29fe17928ee59dc468333241d
gongfuzhu <13368172379@163.com>
2025-08-11 15:52:46 +0800

    Revert "优化"
    This reverts commit 95c43fd16da582ed1e0623310d0f8bc6bd29f82b.

--- demo/GetPlatformItemInfo/platforms/reply_messages/douyin_reply_message.py
+++ demo/GetPlatformItemInfo/platforms/reply_messages/douyin_reply_message.py
@@ -136,13 +136,8 @@ async def run_work(context: BrowserContext, ai_url: str, max_chat_count: int):
 
     # 打开 Douyin 主页
     await page.goto("https://www.douyin.com/")
-
-    async def loop_close_xdg_open_window_handel():
-        while True:
-            await close_xdg_open_window_handel()
-            await asyncio.sleep(1)
-
-    asyncio.create_task(loop_close_xdg_open_window_handel())
+    # 弹窗加载时间
+    await asyncio.sleep(6)
     # 找到取消按钮并点击
     find_and_click_image("res/cancel_button.png", threshold=0.9)
     await page.locator("text=取消").wait_for()
@@ -234,10 +229,6 @@ async def run_work(context: BrowserContext, ai_url: str, max_chat_count: int):
             print("无更多会话，结束循环")
             break
 
-
-async def close_xdg_open_window_handel():
-    find_and_click_image('res/cancel_button.png', threshold=0.9)
-    pass
 
 def add_user_event(user_id):
     """给用户ID计数 +1"""
